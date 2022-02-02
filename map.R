0eced2d630da8b26e20f0feda12044873243e809
Mylène <m-b-c@mail.com>
2022-02-02 19:26:47 +0100

    Adding the chloropleth map

--- map.R
+++ map.R
@@ -1,25 +1,26 @@
-install.packages("gglplot2")
-install.packages("tidyverse")
 
 library(ggplot2)
 library(tidyverse)
 library(dplyr)
 library(tidyr)
 
-results <- read.csv2("/Users/mylene/EDML/Cassowary/processed data.csv", sep=";")
-view(results)
+results = read.csv2("/Users/mylene/EDML/Cassowary/country_text.csv")
+
 map_data <- map_data("world")
-view(map_data)
+
 map_data <- left_join(map_data, results, by="region")
-view(map_data)
-# map_data1 <- map_data %>% filter(!is.na(map_data$subregion))
-#map_data1 <- map_data[!apply(is.na(map_data) | map_data == "", 1, all),]
-#map_data1 <- map_data %>% filter(!is.na(map_data$age))
-#map_data1 <- map_data %>% filter(!is.na(map_data$gender))
 
-map_data1 %>% filter_all(all_vars(!is.na(.)))
-map_data1 %>% filter_all(all_vars(complete.cases(.)))  
-view(map_data1)
 
-map1 <- ggplot(map_data1, aes(x = long, y = lat, group = group)) + geom_polygon(aes(fill = education), color = "blue")
+map1 <- ggplot(map_data, aes(x = long, y = lat, group = group)) + geom_polygon(aes(fill = Population_Dispersion), color = "black")
 map1
+
+map2 <- map1 + scale_fill_gradient(name = "No of participants", low = "pink", high = "purple", na.value = "lightblue") +
+    theme(axis.text.x = element_blank(),
+          axis.text.y = element_blank(),
+          axis.ticks = element_blank(),
+          axis.title.y = element_blank(),
+          axis.title.x = element_blank(),
+          rect = element_blank())
+map2
+
+
